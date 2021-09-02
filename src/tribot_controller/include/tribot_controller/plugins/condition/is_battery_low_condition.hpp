9ab2fb301a42f9c189354403a83dd9faadb5c4a5
Harshil Bhatt <harshilbhatt2001@gmail.com>
2021-09-02 18:42:24 +0530

    battery_low condition node for BT

---
+++ src/tribot_controller/include/tribot_controller/plugins/condition/is_battery_low_condition.hpp
@@ -0,0 +1,55 @@
+#ifndef TRIBOT_CONTROLLER__PLUGINS__CONDITION__IS_BATTERY_LOW_CONDITION_HPP_
+#define TRIBOT_CONTROLLER__PLUGINS__CONDITION__IS_BATTERY_LOW_CONDITION_HPP_
+
+#include <memory>
+#include <string>
+#include <mutex>
+
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/battery_state.hpp"
+#include "behaviortree_cpp_v3/condition_node.h"
+
+namespace nav2_behavior_tree 
+{
+class IsBatteryLowCondition : public BT::ConditionNode
+{
+public:
+    IsBatteryLowCondition(
+        const std::string &condition_name,
+        const BT::NodeConfiguration &conf);
+    
+    IsBatteryLowCondition() = delete;
+
+    BT::NodeStatus tick() override;
+
+    static BT::PortsList providedPorts()
+    {
+        return {
+            BT::InputPort<double>("min_battery", "Minimum battery percentage/voltage"),
+            BT::InputPort<std::string>(
+                "battery_topic", std::string("/battery_status"), "Battery topic"),
+            BT::InputPort<bool>(
+                "is_voltage", false, "If true voltage will be used to check for low battery"),
+        };
+    }
+
+private:
+    /**
+     * @brief Callback function for battery topic
+     * @param msg Shared pointer to sensor_msgs::msg::BatteryState message
+     */
+    void batteryCallback(sensor_msgs::msg::BatteryState::SharedPtr msg);
+
+    rclcpp::Node::SharedPtr node_;
+    rclcpp::CallbackGroup::SharedPtr callback_group_;
+    rclcpp::executors::SingleThreadedExecutor callback_group_executor_;
+    rclcpp::Subscription<sensor_msgs::msg::BatteryState>::SharedPtr battery_sub_;
+    std::string battery_topic_;
+    double min_battery_;
+    bool is_voltage_;
+    bool is_battery_low_;
+};
+
+} // namespace nav2_behavior_tree 
+
+#endif // TRIBOT_CONTROLLER__PLUGINS__CONDITION__IS_BATTERY_LOW_CONDITION_HPP_
